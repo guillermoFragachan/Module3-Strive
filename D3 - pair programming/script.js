8c20948d12c5bf2802d5eed7b8cab614896845d8
guillermoFragachan <guillefragachan@gmail.com>
2021-09-15 14:24:22 +0200

    js

---
+++ D3 - pair programming/script.js
@@ -0,0 +1,26 @@
+console.log()
+
+
+import { METHODS } from 'http'
+// API key:  563492ad6f917000010000019b5a12d6ac8c42e39efb9e162087db71
+
+
+import { createClient } from 'pexels'
+
+const client = createClient('563492ad6f917000010000019b5a12d6ac8c42e39efb9e162087db71')
+
+
+fetch('https://api.pexels.com/v1/'){
+   Authorization: '563492ad6f917000010000019b5a12d6ac8c42e39efb9e162087db71'
+}
+// All requests made with the client will be authenticated
+
+
+//fetch images
+
+//add them to the layout
+
+//changeedit to hide
+
+
+//addevent listener 
