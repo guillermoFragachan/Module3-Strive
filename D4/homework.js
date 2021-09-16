588860edaacc20557d8e41ad6573ad696b519b7c
guillermoFragachan <guillefragachan@gmail.com>
2021-09-16 16:52:35 +0200

    commit

--- D4/homework.js
+++ D4/homework.js
@@ -1,4 +1,4 @@
-let books = []
+let array = []
 
 fetch("https://striveschool-api.herokuapp.com/books", {
 
@@ -13,12 +13,13 @@ fetch("https://striveschool-api.herokuapp.com/books", {
             let card = document.createElement('div')
             card.classList.add('col-3', 'mx-1')
             card.innerHTML = `
-            <div class="card" ;">
+            <div class="card"  >
                 <img class="card-img-top" src="${element.img}" alt="Card image cap">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h6 class="card-title">${element.title}</h6>
+                    <p>${element.price}</p>
+                    <a href="#" class="btn btn-primary" id='${element.asin}' onclick="remove(event, 0)">Add to cart</a>
+                    <a href="#" class="btn btn-secondary mt-1" onclick="remove(event, 1)">Skip</a>
                 </div>
             </div>
             
@@ -26,10 +27,41 @@ fetch("https://striveschool-api.herokuapp.com/books", {
             `
             let bodyNode = document.getElementsByClassName('row')[0]
             bodyNode.appendChild(card)
-            console.log(element)
+            
+            
+            array.push(element) 
+            
         })
 
 
     })
 
+    
+
+
+    function remove(_event, _value){
+        _event.target.closest('.col-3').remove()
+
+
+        let card = _event.target.parentElement.children[0]
+
+        let liNode = document.createElement('li')
+        liNode.classList.add('list-group-item')
+        liNode.appendChild(card)
+
+    
+        
+        
+        let bodyNode = document.getElementsByClassName('list-group')[_value]
+        bodyNode.appendChild(liNode)
+
+        
+
+
+    }
+
+
+    function addToCart(){
+
+    }
 
